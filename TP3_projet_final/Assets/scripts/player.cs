eb6a1b06593995ff1768b068656739b559a4e986
Kliraaa <2472632@cmontmorency.qc.ca>
2026-05-12 13:28:57 -0400

    sounds to potions and scripts to player in interrior scene

---
+++ TP3_projet_final/Assets/scripts/player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public AudioSource potionSound;
+    public int count;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("potion"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            potionSound.Play();
+        }
+    }
+}
