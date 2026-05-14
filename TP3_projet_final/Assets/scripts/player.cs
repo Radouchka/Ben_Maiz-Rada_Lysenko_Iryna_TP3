6e2ad524dce00ddce71491a0151d0f11c6e4f052
Radouchka <2475818@cmontmorency.qc.ca>
2026-05-13 23:28:59 -0400

    teleportation success dans scene rampars

--- TP3_projet_final/Assets/scripts/player.cs
+++ TP3_projet_final/Assets/scripts/player.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class player : MonoBehaviour
 {
+    public string sceneName;
     public AudioSource potionSound;
     public int count;
+    public GameObject lastPotion;
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Collided with: " + other.gameObject.name);
+
         if (other.CompareTag("potion"))
         {
             other.gameObject.SetActive(false);
             count++;
             potionSound.Play();
+        }
+
+        if (other.CompareTag("castle"))
+        {
+            SceneManager.LoadScene(sceneName);
         }
     }
 }
